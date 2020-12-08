4e60532a20f7e062169b308fb12914f3b400a2dc
harshitksrivastava <harsh0311@gmail.com>
2020-12-09 00:31:54 +0530

    Day 13 of 100 Fixed array_count_inversion.py

--- array_count_inversion.py
+++ array_count_inversion.py
@@ -1,3 +1,8 @@
+# Given an array of integers. Find the Inversion Count in the array.
+# For an array, inversion count indicates how far (or close) the array is from being sorted. If array is already
+# sorted then the inversion count is 0. If an array is sorted in the reverse order then the inversion count is the
+# maximum. Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+
 def merge(array, left, mid, right):
     n1 = mid - left + 1
     n2 = right - mid
@@ -18,7 +23,7 @@ def merge(array, left, mid, right):
             k += 1
         else:
             array[k] = R[j]
-            inv_count += (mid - i) + 1
+            inv_count += (mid - i-left) + 1
             j += 1
             k += 1
     while i < n1:
@@ -48,7 +53,19 @@ def count_inversion(array):
 
 
 if __name__ == "__main__":
-    input_array = [8, 4, 2, 1]
+    input_array = [1, 20, 6, 4, 5]
     count = count_inversion(input_array)
     print(input_array)
     print(count)
+
+
+# Input: N = 5, arr[] = {2, 4, 1, 3, 5}
+# Output: 3
+# Explanation: The sequence 2, 4, 1, 3, 5
+# has three inversions (2, 1), (4, 1), (4, 3).
+#
+# Input: N = 5
+# arr[] = {2, 3, 4, 5, 6}
+# Output: 0
+# Explanation: As the sequence is already
+# sorted so there is no inversion count.
