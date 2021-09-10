8755e961f4a1a4465b7489da137a6d0996101b6d
harshitksrivastava <harsh0311@gmail.com>
2021-09-11 03:39:55 +0530

    updated merge_intervals.py

--- LeetCode/merge_intervals.py
+++ LeetCode/merge_intervals.py
@@ -7,6 +7,8 @@
 class Solution:
 
     def merge(self, intervals):
+
+        # sorting based on the first value of every in the intervals
         intervals.sort(key=lambda x: x[0])
         temp = [intervals[0]]
 
@@ -26,3 +28,14 @@ if __name__ == "__main__":
     # ans = sol.merge([[1, 3], [2, 6], [8, 10], [15, 18]])
     ans = sol.merge([[1, 4], [4, 5]])
     print(ans)
+
+
+# Example 1:
+# Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+# Output: [[1,6],[8,10],[15,18]]
+# Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+#
+# Example 2:
+# Input: intervals = [[1,4],[4,5]]
+# Output: [[1,5]]
+# Explanation: Intervals [1,4] and [4,5] are considered overlapping.
